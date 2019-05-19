fbbca926f737ce25215a9cc49cb4af8476bdf901
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-05-19 15:32:41 +0900

    fixed schema

--- db/isubata.sql
+++ db/isubata.sql
@@ -28,7 +28,7 @@ CREATE TABLE message (
   user_id BIGINT,
   content TEXT,
   created_at DATETIME NOT NULL,
-  INDEX user_id_idx (user_id),
+  INDEX user_id_idx (user_id)
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE haveread (
