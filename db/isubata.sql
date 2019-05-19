0bb29e03efc28f4d14e0334f8172e6f5c7972e65
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-05-19 17:28:05 +0900

    fix index

--- db/isubata.sql
+++ db/isubata.sql
@@ -13,7 +13,7 @@ CREATE TABLE image (
   id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
   name VARCHAR(191),
   data LONGBLOB,
-  PRIMARY KEY name_idx (name)
+  KEY name_idx (name)
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE channel (
