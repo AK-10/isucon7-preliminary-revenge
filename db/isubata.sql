2fae52cf53672d556c96a5c49d9e05cda23794f7
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-06-09 01:03:37 +0900

    fixed-schema

--- db/isubata.sql
+++ db/isubata.sql
@@ -11,7 +11,8 @@ CREATE TABLE user (
 CREATE TABLE image (
   id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
   name VARCHAR(191),
-  data LONGBLOB
+  data LONGBLOB,
+  KEY name_index_on_image(name)
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE channel (
@@ -28,7 +29,8 @@ CREATE TABLE message (
   user_id BIGINT,
   content TEXT,
   created_at DATETIME NOT NULL,
-  KEY user_id_idx (user_id)
+  KEY channel_id_index_on_message(channel_id),
+  KEY user_id_index_on_message(user_id)
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE haveread (
@@ -38,6 +40,5 @@ CREATE TABLE haveread (
   updated_at DATETIME NOT NULL,
   created_at DATETIME NOT NULL,
   PRIMARY KEY(user_id, channel_id),
-  KEY channel_id_idx (channel_id),
-  KEY message_id_idx (message_id)
+  KEY message_index_on_haveread(message_id)
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
