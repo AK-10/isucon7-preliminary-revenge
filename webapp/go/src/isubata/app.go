e62a4e8ca7b8bbaf13e08fd4ae5066b26b7f5b2f
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-06-09 04:17:41 +0900

    rename getHistoryV2

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -339,7 +339,7 @@ func fetchUnread(c echo.Context) error {
 	return c.JSON(http.StatusOK, resp)
 }
 
-func getHistoryV2(c echo.Context) error {
+func getHistory(c echo.Context) error {
 	chID, err := strconv.ParseInt(c.Param("channel_id"), 10, 64)
 	if err != nil || chID <= 0 {
 		return ErrBadReqeust
@@ -416,75 +416,75 @@ func getHistoryV2(c echo.Context) error {
 	})
 }
 
-func getHistory(c echo.Context) error {
-	chID, err := strconv.ParseInt(c.Param("channel_id"), 10, 64)
-	if err != nil || chID <= 0 {
-		return ErrBadReqeust
-	}
+// func getHistory(c echo.Context) error {
+// 	chID, err := strconv.ParseInt(c.Param("channel_id"), 10, 64)
+// 	if err != nil || chID <= 0 {
+// 		return ErrBadReqeust
+// 	}
 
-	user, err := ensureLogin(c)
-	if user == nil {
-		return err
-	}
+// 	user, err := ensureLogin(c)
+// 	if user == nil {
+// 		return err
+// 	}
 
-	var page int64
-	pageStr := c.QueryParam("page")
-	if pageStr == "" {
-		page = 1
-	} else {
-		page, err = strconv.ParseInt(pageStr, 10, 64)
-		if err != nil || page < 1 {
-			return ErrBadReqeust
-		}
-	}
+// 	var page int64
+// 	pageStr := c.QueryParam("page")
+// 	if pageStr == "" {
+// 		page = 1
+// 	} else {
+// 		page, err = strconv.ParseInt(pageStr, 10, 64)
+// 		if err != nil || page < 1 {
+// 			return ErrBadReqeust
+// 		}
+// 	}
 
-	const N = 20
-	var cnt int64
-	err = db.Get(&cnt, "SELECT COUNT(id) as cnt FROM message WHERE channel_id = ?", chID)
-	if err != nil {
-		return err
-	}
-	maxPage := int64(cnt+N-1) / N
-	if maxPage == 0 {
-		maxPage = 1
-	}
-	if page > maxPage {
-		return ErrBadReqeust
-	}
+// 	const N = 20
+// 	var cnt int64
+// 	err = db.Get(&cnt, "SELECT COUNT(id) as cnt FROM message WHERE channel_id = ?", chID)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	maxPage := int64(cnt+N-1) / N
+// 	if maxPage == 0 {
+// 		maxPage = 1
+// 	}
+// 	if page > maxPage {
+// 		return ErrBadReqeust
+// 	}
 
-	messages := []Message{}
-	err = db.Select(&messages,
-		"SELECT * FROM message WHERE channel_id = ? ORDER BY id DESC LIMIT ? OFFSET ?",
-		chID, N, (page-1)*N)
-	if err != nil {
-		return err
-	}
+// 	messages := []Message{}
+// 	err = db.Select(&messages,
+// 		"SELECT * FROM message WHERE channel_id = ? ORDER BY id DESC LIMIT ? OFFSET ?",
+// 		chID, N, (page-1)*N)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	// N+1
-	mjson := make([]map[string]interface{}, 0)
-	for i := len(messages) - 1; i >= 0; i-- {
-		r, err := jsonifyMessage(messages[i])
-		if err != nil {
-			return err
-		}
-		mjson = append(mjson, r)
-	}
+// 	// N+1
+// 	mjson := make([]map[string]interface{}, 0)
+// 	for i := len(messages) - 1; i >= 0; i-- {
+// 		r, err := jsonifyMessage(messages[i])
+// 		if err != nil {
+// 			return err
+// 		}
+// 		mjson = append(mjson, r)
+// 	}
 
-	channels := []ChannelInfo{}
-	err = db.Select(&channels, "SELECT * FROM channel ORDER BY id")
-	if err != nil {
-		return err
-	}
+// 	channels := []ChannelInfo{}
+// 	err = db.Select(&channels, "SELECT * FROM channel ORDER BY id")
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return c.Render(http.StatusOK, "history", map[string]interface{}{
-		"ChannelID": chID,
-		"Channels":  channels,
-		"Messages":  mjson,
-		"MaxPage":   maxPage,
-		"Page":      page,
-		"User":      user,
-	})
-}
+// 	return c.Render(http.StatusOK, "history", map[string]interface{}{
+// 		"ChannelID": chID,
+// 		"Channels":  channels,
+// 		"Messages":  mjson,
+// 		"MaxPage":   maxPage,
+// 		"Page":      page,
+// 		"User":      user,
+// 	})
+// }
 
 func getProfile(c echo.Context) error {
 	self, err := ensureLogin(c)
@@ -662,8 +662,8 @@ func main() {
 	e.GET("/message", getMessage)
 	e.POST("/message", postMessage)
 	e.GET("/fetch", fetchUnread)
-	// e.GET("/history/:channel_id", getHistory)
-	e.GET("/history/:channel_id", getHistoryV2)
+	e.GET("/history/:channel_id", getHistory)
+	// e.GET("/history/:channel_id", getHistoryV2)
 	e.GET("/profile/:user_name", getProfile)
 	e.POST("/profile", postProfile)
 
