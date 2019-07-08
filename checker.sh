750057fb15d2bd2d895750f3c5e2f51fabbe04c0
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-07-09 00:07:34 +0900

    fix checker.sh

--- checker.sh
+++ checker.sh
@@ -1,5 +1,8 @@
 cd ~/isubata/webapp/go
 make
+sudo rm /var/log/nginx/access.log
+sudo systemctl restart nginx
+sudo systemctl restart redis
 sudo systemctl restart isubata.golang.service
 cd ~/isubata/bench
 ./bin/bench -remotes=127.0.0.1 -output result.json
