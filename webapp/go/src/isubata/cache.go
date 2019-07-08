4c9c074fcd1199949e760c0dc46dc8d92fca81ad
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-07-09 02:51:48 +0900

    cancel redis connection pool

--- webapp/go/src/isubata/cache.go
+++ webapp/go/src/isubata/cache.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"strconv"
-	"time"
 
 	"github.com/gomodule/redigo/redis"
 )
@@ -13,14 +12,14 @@ const (
 	userNumKey       = "USER-NUM"
 )
 
-var (
-	redisPool = &redis.Pool{
-		MaxIdle:     3,
-		MaxActive:   18,
-		IdleTimeout: 240 * time.Second,
-		Dial:        func() (redis.Conn, error) { return redis.Dial("tcp", "127.0.0.1:6379") },
-	}
-)
+// var (
+// 	redisPool = &redis.Pool{
+// 		MaxIdle:     3,
+// 		MaxActive:   18,
+// 		IdleTimeout: 240 * time.Second,
+// 		Dial:        func() (redis.Conn, error) { return redis.Dial("tcp", "127.0.0.1:6379") },
+// 	}
+// )
 
 func makeMSGNumKey(chanID int64) string {
 	return messageNumPrefix + strconv.Itoa(int(chanID))
@@ -31,15 +30,25 @@ func makeHaveReadKey(chanID, userID int64) string {
 }
 
 func setLastIDtoRedis(chanID, userID, lastID int64) error {
-	conn := redisPool.Get()
+	// conn := redisPool.Get()
+	// defer conn.Close()
+	conn, err := redis.Dial("tcp", "localhost:6379")
+	if err != nil {
+		panic(err)
+	}
 	defer conn.Close()
 	key := makeHaveReadKey(chanID, userID)
-	_, err := conn.Do("SET", key, strconv.Itoa(int(lastID)))
+	_, err = conn.Do("SET", key, strconv.Itoa(int(lastID)))
 	return err
 }
 
 func getLastIDFromRedis(chanID, userID int64) (int64, error) {
-	conn := redisPool.Get()
+	// conn := redisPool.Get()
+	// defer conn.Close()
+	conn, err := redis.Dial("tcp", "localhost:6379")
+	if err != nil {
+		panic(err)
+	}
 	defer conn.Close()
 	key := makeHaveReadKey(chanID, userID)
 	lastID, err := redis.Int64(conn.Do("GET", key))
@@ -69,15 +78,25 @@ func decrementMessageNumtoRedis(chanID int64) error {
 }
 
 func setMessageNumFromRedis(chanID, num int64) error {
-	conn := redisPool.Get()
+	// conn := redisPool.Get()
+	// defer conn.Close()
+	conn, err := redis.Dial("tcp", "localhost:6379")
+	if err != nil {
+		panic(err)
+	}
 	defer conn.Close()
 	key := makeMSGNumKey(chanID)
-	_, err := conn.Do("SET", key, strconv.Itoa(int(num)))
+	_, err = conn.Do("SET", key, strconv.Itoa(int(num)))
 	return err
 }
 
 func getMessageNumFromRedis(chanID int64) (int64, error) {
-	conn := redisPool.Get()
+	// conn := redisPool.Get()
+	// defer conn.Close()
+	conn, err := redis.Dial("tcp", "localhost:6379")
+	if err != nil {
+		panic(err)
+	}
 	defer conn.Close()
 	key := makeMSGNumKey(chanID)
 	num, err := redis.Int64(conn.Do("GET", key))
@@ -96,15 +115,25 @@ func incrementUserNumtoRedis() error {
 }
 
 func setUserNumFromRedis(num int64) error {
-	conn := redisPool.Get()
+	// conn := redisPool.Get()
+	// defer conn.Close()
+	conn, err := redis.Dial("tcp", "localhost:6379")
+	if err != nil {
+		panic(err)
+	}
 	defer conn.Close()
 	key := userNumKey
-	_, err := conn.Do("SET", key, strconv.Itoa(int(num)))
+	_, err = conn.Do("SET", key, strconv.Itoa(int(num)))
 	return err
 }
 
 func getUserNumFromRedis() (int64, error) {
-	conn := redisPool.Get()
+	// conn := redisPool.Get()
+	// defer conn.Close()
+	conn, err := redis.Dial("tcp", "localhost:6379")
+	if err != nil {
+		panic(err)
+	}
 	defer conn.Close()
 	key := userNumKey
 	num, err := redis.Int64(conn.Do("GET", key))
